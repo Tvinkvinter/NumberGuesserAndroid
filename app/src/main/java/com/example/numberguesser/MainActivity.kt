4da6c843cb0d6af84fbf9e6f3c6699d6eb715cc0
Tvinkvinter <Tvinkvinter@gmail.com>
2023-04-07 20:52:48 +0300

    Add language spinner to menu

--- app/src/main/java/com/example/numberguesser/MainActivity.kt
+++ app/src/main/java/com/example/numberguesser/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.numberguesser
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.SimpleAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.GravityCompat
 import com.example.numberguesser.databinding.ActivityMainBinding
 import com.google.android.material.switchmaterial.SwitchMaterial
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     lateinit var preferences: SharedPreferences
@@ -23,8 +25,6 @@ class MainActivity : AppCompatActivity(){
         options = savedInstanceState?.getParcelable(KEY_OPTIONS) ?: Options.DEFAULT
         binding.openMenuButton.setOnClickListener {
             binding.drawer.openDrawer(GravityCompat.END)
-            //menuButtonAnim = AnimationUtils.loadAnimation(applicationContext, R.anim.move_left)
-            //it.startAnimation(menuButtonAnim)
         }
 
         setMenu()
@@ -61,19 +61,44 @@ class MainActivity : AppCompatActivity(){
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
             }
         }
+
+        val spinner = binding.settingsMenu.menu.findItem(R.id.spinner_item).actionView as Spinner
+        val data = listOf(
+            mapOf(
+                KEY_TITLE to getString(R.string.eng_lang),
+                KEY_ICON to R.drawable.eng_lang_icon
+            ),
+            mapOf(
+                KEY_TITLE to getString(R.string.es_lang),
+                KEY_ICON to R.drawable.es_lang_icon
+            ),
+            mapOf(
+                KEY_TITLE to getString(R.string.rus_lang),
+                KEY_ICON to R.drawable.ru_lang_icon
+            ),
+            mapOf(
+                KEY_TITLE to getString(R.string.zh_lang),
+                KEY_ICON to R.drawable.zh_lang_icon
+            )
+        )
+
+
+        val adapter = SimpleAdapter(
+            this,
+            data,
+            R.layout.spinner_row,
+            arrayOf(KEY_TITLE, KEY_ICON),
+            intArrayOf(R.id.text_lang, R.id.icon_lang)
+        )
+        spinner.adapter = adapter
     }
 
     companion object {
-        @JvmStatic
-        private val KEY_OPTIONS = "OPTIONS"
-
-        @JvmStatic
-        private val APP_PREFERENCES = "APP_PREFERENCES"
-
-        @JvmStatic
-        private val PREF_DARK_MODE = "PREF_DARK_MODE"
-
-        @JvmStatic
-        val PREF_SLIDER_VAL = "PREF_SLIDER_VAL"
+        private const val KEY_OPTIONS = "OPTIONS"
+        private const val APP_PREFERENCES = "APP_PREFERENCES"
+        private const val PREF_DARK_MODE = "PREF_DARK_MODE"
+        const val PREF_SLIDER_VAL = "PREF_SLIDER_VAL"
+        private const val KEY_TITLE = "LANGUAGE"
+        private const val KEY_ICON = "ICON"
     }
 }
